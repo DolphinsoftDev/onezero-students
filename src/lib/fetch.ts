55348941d75ca5a17b0dd9cc8d9e27fe238d45b0
kobiregev <regev79@gmail.com>
2023-05-17 14:31:22 +0300

    fix: base url

--- src/lib/fetch.ts
+++ src/lib/fetch.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://marketing.devfdb.net/api/v1/marketing/",
+  baseURL: "https://www.onezerobank.com/api/v1/marketing/",
 });
 
 export default instance;
